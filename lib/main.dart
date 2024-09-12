55ef298335eaf4ee38263b73767d9aa2b8fba3b1
Ali <ali.akbar@serafin-am.com>
2024-09-12 10:21:45 +0200

    Init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_frontend/feat/startup/presentation/page/start_up_page/start_up_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Todo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const StartUpPage());
+  }
+}
