4587e2d7d18924689efc3f1a4922ba1f1b3b0e8e
Ali <ali.akbar@serafin-am.com>
2024-09-12 10:31:19 +0200

    feat: add packages

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Todo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const StartUpPage());
+      title: 'Todo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const StartUpPage(),
+    );
   }
 }
